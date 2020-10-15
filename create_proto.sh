f1b5e3682a912d9877c0d7b71a46bc45f2f9c402
yun_sunjin <yun_sunjin@cocone.co.jp>
2020-10-15 18:33:43 +0900

    script

--- create_proto.sh
+++ create_proto.sh
@@ -1,9 +1,5 @@
 #!/bin/sh -ex
 
-# git submodule
-# git submodule init
-# git submodule update
-
 DIR=`pwd`
 
 cd ..
@@ -16,15 +12,6 @@ mkdir -p $DIR/src/infra/grpc/proto
 export PATH="$PATH:$HOME/go/bin"
 
 cd $DIR/proto
-# find . -type d -name vendor -prune -o -type f -name "*.proto" | xargs echo # ./vendorも読み込まれてるから変なメッセージでるみたい、どうにかしたい
-# find . -type d -name vendor -prune -o -type f -name "*.proto" | xargs --replace=AlmaProtoFile protoc --proto_path=. -Ivendor/protobuf/src AlmaProtoFile --go_out=plugins=grpc:${OUT_DIR}
-
-
-# find . -type d -name vendor -prune -o -type f -name "*.proto" -exec echo {} \; | xargs --replace=AlmaProtoFile protoc --proto_path=. -Ivendor/protobuf/src AlmaProtoFile --go_out=plugins=grpc:${OUT_DIR}
-# find . -type d -name vendor -prune -o -type f -name "*.proto" -exec echo {} \; | xargs --replace=AlmaProtoFile protoc --proto_path=. -Ivendor/protobuf/src AlmaProtoFile --go_out=plugins=grpc:${OUT_DIR}
-
-# macはreplace optionをサポートしていない
-# find . -type d -name vendor -prune -o -type f -name "*.proto" -exec echo {} \; | xargs -I AlmaProtoFile protoc --proto_path=. -Ivendor/protobuf/src AlmaProtoFile --go_out=plugins=grpc:${OUT_DIR}
 find . -name "*.proto" -exec protoc --proto_path . --go_out=plugins=grpc:$OUT_DIR {} \;
 
 echo "==== create proto fin ===="
